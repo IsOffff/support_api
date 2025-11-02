bbe4e46c86e86a08a9160480d784a6ca3c36eb0b
Isaac <diarraisaac312@icloud.com>
2025-11-02 12:49:15 +0100

    mis à jour de eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest, 
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      quotes: ["warn", "double"],
+      semi: ["error", "always"],
+      indent: ["error", 2],
+    },
+  },
+];
